4f06db7afc3b8143b66703f594753d8a2be323f1
unknown <tompsherman@gmail.com>
2020-09-28 15:35:22 -0400

    set up initial todo list and render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import TodoList from "./components/TodoList"
+
+const todo = [
+  {
+  name: "dishes",
+  id: 1234,
+  completed: false
+  },
+  {
+    name: "mow lawn",
+    id: 1235,
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor( ){
+    super()
+    this.state ={
+      todo
+    }
+  }
+
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todo={this.state.todo}/>
       </div>
     );
   }
